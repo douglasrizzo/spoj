9b399d4aa174da9a33912162a1cf71f18810f9e1
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2014-02-02 20:48:40 -0200

    Primeiro envio

---
+++ CMPLS.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stack>
+#include <queue>
+#include <iostream>
+
+int main()
+{
+    int t, s, c;
+
+    //BACKWARDS INTERPOLATION - AC
+    //Babbage's Difference Engine - AC
+
+    scanf("%d", &t);
+    while(t--)
+    {
+
+    return 0;
+}
+
---
+++ PALIN.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+// https://foolishmoron.wordpress.com/2009/09/28/the-next-palindrome/#more-23
+
+int main()
+{
+    int t;
+    string m;
+
+    //captura iterações
+    scanf("%d", &t);
+    while(t--)
+    {
+        std::cin>>m;
+
+
+    }
+        return 0;
+}
