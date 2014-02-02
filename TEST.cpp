792bc96626b0d2d9d77a38b1088a53b1086eaf28
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2014-02-02 20:54:23 -0200

    Descomentadas as linhas

--- TEST.cpp
+++ TEST.cpp
@@ -1,7 +1,7 @@
-//#include <stdio.h>
-//
-//int main(void) {
-//  int x;
-//  for(; scanf("%d",&x) > 0 && x != 42; printf("%d\n", x));
-//  return 0;
-//}
+#include <stdio.h>
+
+int main(void) {
+  int x;
+  for(; scanf("%d",&x) > 0 && x != 42; printf("%d\n", x));
+  return 0;
+}
