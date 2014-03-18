9a90a7be57175a993beed7845ae2ef8b4ddd4bc4
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2014-03-18 13:55:47 -0300

    Create ADDREV.cpp

---
+++ acc/ADDREV.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int Reverse(int num_)
+{
+        int inv; inv = 0;
+
+        while (num_ > 0)
+        {
+                inv = inv * 10 + (num_%10);
+                num_ /= 10;
+        }
+
+        return inv;
+}
+
+int main()
+{
+    int t,n,m;
+
+    scanf("%d", &t);
+    while(t--)
+    {
+        scanf("%d %d", &m, &n);
+        printf("%d\n", Reverse(Reverse(m)+Reverse(n)));
+    }
+    return 0;
+}
